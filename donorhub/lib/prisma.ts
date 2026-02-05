66281ece4d657e461b2982f56d8fdbe9a12ce83f
Sanjana Purbey <sanjanapurbey@Sanjanas-MacBook-Air.local>
2026-02-05 18:00:11 +0530

    Use direct Prisma datasource URL instead of Accelerate

--- donorhub/lib/prisma.ts
+++ donorhub/lib/prisma.ts
@@ -1,20 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
   const databaseUrl = process.env.DATABASE_URL;
-  
+
   if (!databaseUrl) {
     throw new Error("DATABASE_URL environment variable is not set");
   }
 
   return new PrismaClient({
-    accelerateUrl: databaseUrl,
-  }).$extends(withAccelerate());
+    datasourceUrl: databaseUrl,
+  });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
